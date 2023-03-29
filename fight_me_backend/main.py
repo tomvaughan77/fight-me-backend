59188e209a4fbefc5cfe9dbd4b9671dc62455e91
Tom Vaughan <tomvaughan77@gmail.com>
2023-03-29 15:35:38 +0100

    Broadcasts the number of arguing users every 2 seconds

--- fight_me_backend/main.py
+++ fight_me_backend/main.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 import uvicorn
-
 import socketio
+import asyncio
+
+from threading import Thread
 
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
 app = socketio.ASGIApp(sio)
 
 
-users = []
+users = {}
 room = "test_room"
 messages = {}
+
+def broadcast_message():
+    message = "Hello, clients!"
+    socketio.emit('broadcast_message', message, broadcast=True)
 
 
 @sio.on("connect")
 def test_connect(sid, data):
     print(f"⚡: {sid} user just connected!")
-    users.append(sid)
+    users[sid] = { "room": None }
     print(users)
 
 
@@ -23,7 +29,7 @@ def test_connect(sid, data):
 async def test_disconnect(sid):
     print(f"🔥: {sid} user disconnected")
     sio.leave_room(sid, room)
-    users.remove(sid)   
+    users.pop(sid)
     await sio.disconnect(sid)
     print(users)
 
@@ -54,6 +60,7 @@ async def getMessages(sid, data):
 async def getRoom(sid):
     print(f"Adding SID {sid} to room {room}")
     sio.enter_room(sid, room)
+    users[sid]["room"] = room
 
     await sio.emit("getRoomResponse", { "room": room })
     
@@ -62,8 +69,33 @@ async def getRoom(sid):
 async def leaveRoom(sid, data):
     print(f"Removing SID {sid} from room {data['room']}")
     sio.leave_room(sid, room)
+    users[sid]["room"] = None
     await sio.emit("leaveRoomResponse", { "room": data['room'] })
 
+
+async def connected_users():
+    users_in_rooms = sum(1 for _, user_data in users.items() if user_data.get('room') is not None)
+    await sio.emit("connectedUsers", users_in_rooms)
+
+async def connected_users_timer(interval_seconds):
+    while True:
+        await connected_users()
+        await asyncio.sleep(interval_seconds)
+
+def start_connected_users_timer():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.create_task(connected_users_timer(2))
+        loop.run_forever()
+    except KeyboardInterrupt:
+        loop.stop()
+    finally:
+        loop.close()
+
+
+thread = Thread(target=start_connected_users_timer, daemon=True)
+thread.start()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=5000)
