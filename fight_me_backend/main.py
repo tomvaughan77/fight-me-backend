919df63282fde1dc6003796fafc1f81648308f18
Tom Vaughan <tomvaughan77@gmail.com>
2023-03-29 11:27:47 +0100

    Further uvicorn fixes, also better connect/disconnect logic

--- fight_me_backend/main.py
+++ fight_me_backend/main.py
@@ -2,6 +2,7 @@
 import uvicorn
 
 import socketio
+import json
 
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
 app = socketio.ASGIApp(sio)
@@ -11,33 +12,26 @@ users = []
 
 
 @sio.on("connect")
-def test_connect(sid):
+def test_connect(sid, data):
     print(f"⚡: {sid} user just connected!")
+    users.append(sid)
     print(users)
 
 
 @sio.on("disconnect")
-def test_disconnect(sid):
+async def test_disconnect(sid):
     print(f"🔥: {sid} user disconnected")
-    users.remove({"socketID": sid})
-    sio.emit("newUserResponse", users)
-    sio.disconnect()
-
-
-@sio.on("newUser")
-def new_user(data):
-    users.push(data)
-    sio.emit("newUserResponse", users)
+    users.remove(sid)   
+    await sio.disconnect(sid)
+    print(users)
 
 
 @sio.event
-def message(data):
-    sio.emit("messageResponse", data)
-
-
-@sio.event
-def typing(sid, data):
-    sio.emit("typingResponse", data, skip_sid=sid)
+async def message(sid, data):
+    print(f"SID: {sid}")
+    print(data)
+    print(f"Message: {data['text']} from {data['name']}")
+    await sio.emit("messageResponse", data)
 
 
 if __name__ == "__main__":
